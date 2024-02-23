30ef921f4d8c538d3f8c12b788f5dffd357d8255
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-02-24 00:58:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True. otherwise false");
+console.log(car == 'toyo');
+console.log(car == 'subaru');
+var bike = 'Honda';
+console.log("Is bike == Honda? I predict True otherwise false");
+console.log(bike == 'Honda');
+console.log(bike == 'Bionic');
+var fruite = 'Apple';
+console.log("Is fruite == Apple? I predict True otherwise false");
+console.log(fruite == 'Apple');
+console.log(fruite == 'Mango');
+var num = '23';
+console.log("Is number == 23? I predict True otherwise false");
+console.log(num == "43");
+console.log(num == '23');
+var coumputer = 'Dell';
+console.log("Is computer == Dell? I predict True");
+console.log(coumputer == 'Dell');
+console.log(coumputer == 'lenovo');
+var cat = 'Persion';
+console.log("Is cat == 'persion'? I predict True. otherwise false");
+console.log(cat == 'Rusion');
+console.log(cat == 'Persion');
+var dog = 'Huskey';
+console.log("Is dog == Huskey? I predict True otherwise false");
+console.log(dog == 'Huskey');
+console.log(dog == 'Pomeranian');
+var animal = 'lion';
+console.log("Is animal == Lion? I predict True otherwise false");
+console.log(animal == 'tiger');
+console.log(animal == 'lion');
+var country = 'Pakistan';
+console.log("Is country == Pakistan? I predict True otherwise false");
+console.log(country == "karachi");
+console.log(country == 'PAkistan');
+var mobile = 'infinix';
+console.log("Is mobile == infinix? I predict True");
+console.log(mobile == 'Tecno');
+console.log(mobile == 'infinix');
---
+++ main.ts
@@ -0,0 +1,68 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True. otherwise false")
+console.log(car == 'toyo')
+console.log(car == 'subaru')
+
+let bike = 'Honda'
+console.log("Is bike == Honda? I predict True otherwise false")
+console.log( bike == 'Honda')
+console.log( bike == 'Bionic')
+
+
+let fruite = 'Apple'
+console.log("Is fruite == Apple? I predict True otherwise false")
+console.log( fruite == 'Apple')
+console.log( fruite == 'Mango')
+
+
+let num = '23'
+console.log("Is number == 23? I predict True otherwise false")
+console.log( num == "43")
+console.log( num == '23')
+
+
+let  coumputer = 'Dell'
+console.log("Is computer == Dell? I predict True")
+console.log( coumputer == 'Dell')
+console.log( coumputer == 'lenovo')
+
+
+let cat = 'Persion';
+console.log("Is cat == 'persion'? I predict True. otherwise false")
+console.log(cat == 'Rusion')
+console.log(cat == 'Persion')
+
+let dog = 'Huskey'
+console.log("Is dog == Huskey? I predict True otherwise false")
+console.log( dog == 'Huskey')
+console.log( dog == 'Pomeranian')
+
+
+let animal = 'lion'
+console.log("Is animal == Lion? I predict True otherwise false")
+console.log( animal == 'tiger')
+console.log( animal == 'lion')
+
+
+let country = 'Pakistan'
+console.log("Is country == Pakistan? I predict True otherwise false")
+console.log( country == "karachi")
+console.log( country == 'Pakistan')
+
+
+let  mobile = 'infinix'
+console.log("Is mobile == infinix? I predict True")
+console.log( mobile == 'Tecno')
+console.log( mobile == 'infinix')
