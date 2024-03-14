5cb12918f7a2c47a1eb0dfeb0d003f6eedad7b46
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:44:58 +0500

    Add files via upload

---
+++ Qno23.js
@@ -0,0 +1,48 @@
+"use strict";
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1st true condition
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True. otherwise false");
+console.log(car == 'subaru');
+// 2nd true condition
+let bike = 'Honda';
+console.log("Is bike == Honda? I predict True otherwise false");
+console.log(bike == 'Honda');
+// 3rd true condition
+let fruite = 'Apple';
+console.log("Is fruite == Apple? I predict True otherwise false");
+console.log(fruite == 'Apple');
+// 4th true condition
+let num = '23';
+console.log("Is number == 23? I predict True otherwise false");
+console.log(num == '23');
+// 5th true condition
+let coumputer = 'Dell';
+console.log("Is computer == Dell? I predict True");
+console.log(coumputer == 'Dell');
+// 1st false condition
+let cat = 'Persion';
+console.log("Is cat == 'persion'? I predict True. otherwise false");
+console.log(cat == 'Rusion');
+// 2nd false condition
+let dog = 'Huskey';
+console.log("Is dog == Huskey? I predict True otherwise false");
+console.log(dog == 'Afghan kuchi');
+// 3rd false condition
+let animal = 'lion';
+console.log("Is animal == Lion? I predict True otherwise false");
+console.log(animal == 'Tiger');
+// 4th false condition
+let country = 'Pakistan';
+console.log("Is country == Pakistan? I predict True otherwise false");
+console.log(country == 'PAkistan');
+// 5th false condition
+let mobile = 'infinix';
+console.log("Is mobile == infinix? I predict True");
+console.log(mobile == 'Tecno');
